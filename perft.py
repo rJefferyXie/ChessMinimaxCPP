6f50c667ebda20d23b4127238175c8f3427aefb2
JEFFERY XIE <jeffxie27@outlook.com>
2025-03-22 15:34:28 -0600

    Added more positions and tweaked perft to include move types. Move generation is still not working 100%.

--- perft.py
+++ perft.py
@@ -1,25 +1,34 @@
 from game.bitboard import Board
-from constants.fen import STARTING_BOARD
+from constants.fen import STARTING_BOARD, KIWIPETE, POSITION3
+from constants.pieces import SQUARES_MAP
 
 from collections import defaultdict
 
 board = Board()
-board.setup_starting_pieces_from_fen(STARTING_BOARD)
-moves_by_piece_type = defaultdict(int)
+board.setup_starting_pieces_from_fen(KIWIPETE)
+moves_by_type = defaultdict(int)
+moves_by_square = defaultdict(int)
 
 def perft(depth):
   if depth == 0:
     return 1
 
   moves = get_legal_moves()
-  board.current_player_color = 1 if board.current_player_color == 0 else 0
   num_positions = 0
 
   for move in moves:
-    piece = board.get_square_piece(move[0])
-    moves_by_piece_type[piece] += 1
-    
-    board.make_move(move)
+    piece_type = board.get_square_piece(move[0])
+    if board.is_knight(piece_type):
+      moves_by_square["N" + SQUARES_MAP[move[1]]] += 1
+    if board.is_pawn(piece_type):
+      moves_by_square[SQUARES_MAP[move[1]]] += 1
+
+    move_type = board.make_move(move)
+    moves_by_type[move_type] += 1
+
+    if board.king_in_check(board.current_player_color):
+      moves_by_type["checks"] += 1
+
     num_positions += perft(depth - 1)
     board.undo_move()
 
@@ -29,9 +38,9 @@ def get_legal_moves():
   moves = []
   for square in range(64):
     piece_type = board.get_square_piece(square)
-    if not piece_type:
+    if piece_type == None:
       continue
-    
+
     piece_color = 0 if piece_type < 6 else 1
     if piece_color == board.current_player_color:
       for target_pos in board.generate_moves(piece_type, square):
@@ -40,11 +49,32 @@ def get_legal_moves():
   legal_moves = []
   for move in moves:
     board.make_move(move)
-    if not board.king_in_check(board.current_player_color):
+    if not board.king_in_check(1 - board.current_player_color):
       legal_moves.append(move)
     board.undo_move()
   
   return legal_moves
 
+print(perft(1))
+print(moves_by_type)
+# print(sorted(moves_by_square.items(), key=lambda x: (-x[1], x[0])))
+moves_by_type.clear()
+moves_by_square.clear()
+
 print(perft(2))
-print(moves_by_piece_type)
+print(moves_by_type)
+# print(sorted(moves_by_square.items(), key=lambda x: (-x[1], x[0])))
+moves_by_type.clear()
+moves_by_square.clear()
+
+print(perft(3))
+print(moves_by_type)
+# # print(sorted(moves_by_square.items(), key=lambda x: (-x[1], x[0])))
+# moves_by_type.clear()
+# moves_by_square.clear()
+
+# print(perft(4))
+# print(moves_by_type)
+# # print(sorted(moves_by_square.items(), key=lambda x: (-x[1], x[0])))
+# moves_by_type.clear()
+# moves_by_square.clear()
