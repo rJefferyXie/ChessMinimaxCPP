6254bb568bd550d7ffd5863d139caa56c5317441
JEFFERY XIE <jeffxie27@outlook.com>
2025-03-22 15:34:40 -0600

    Added a squares map for perft if we need it.

--- constants/pieces.py
+++ constants/pieces.py
@@ -51,3 +51,14 @@ PIECE_IMAGES = {
   PIECE_BLACK_ROOK: "assets/Black_Rook.png", PIECE_BLACK_BISHOP: "assets/Black_Bishop.png",
   PIECE_BLACK_KNIGHT: "assets/Black_Knight.png", PIECE_BLACK_PAWN: "assets/Black_Pawn.png"
 }
+
+SQUARES_MAP = {
+  0: 'a8', 1: 'b8', 2: 'c8', 3: 'd8', 4: 'e8', 5: 'f8', 6: 'g8', 7: 'h8',
+  8: 'a7', 9: 'b7', 10: 'c7', 11: 'd7', 12: 'e7', 13: 'f7', 14: 'g7', 15: 'h7',
+  16: 'a6', 17: 'b6', 18: 'c6', 19: 'd6', 20: 'e6', 21: 'f6', 22: 'g6', 23: 'h6',
+  24: 'a5', 25: 'b5', 26: 'c5', 27: 'd5', 28: 'e5', 29: 'f5', 30: 'g5', 31: 'h5',
+  32: 'a4', 33: 'b4', 34: 'c4', 35: 'd4', 36: 'e4', 37: 'f4', 38: 'g4', 39: 'h4',
+  40: 'a3', 41: 'b3', 42: 'c3', 43: 'd3', 44: 'e3', 45: 'f3', 46: 'g3', 47: 'h3',
+  48: 'a2', 49: 'b2', 50: 'c2', 51: 'd2', 52: 'e2', 53: 'f2', 54: 'g2', 55: 'h2',
+  56: 'a1', 57: 'b1', 58: 'c1', 59: 'd1', 60: 'e1', 61: 'f1', 62: 'g1', 63: 'h1'
+}
